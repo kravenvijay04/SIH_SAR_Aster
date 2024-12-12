9e2b9a3d193bb0c20d8023d053779175bad112a8
kravenvijay04 <123456789+odin@users.noreply.github.com>
2024-12-13 02:38:19 +0530

    frontend file of SAR_website

---
+++ frontend/src/App.jsx
@@ -0,0 +1,43 @@
+// // eslint-disable-next-line no-unused-vars
+// import React from 'react'
+
+// const App = () => {
+//   // let inputFile = document.getElementById("input-file");
+//   // let display = document.getElementById("display");
+//   let container = document.getElementsByClassName("container");
+//   // let button = document.getElementsByClassName("but");
+//   const Change = () => {
+    
+//     var div = document.createElement("div")
+//     div.setAttribute("class", "bookcont")
+//     div.innerHTML = ` <img src="/frontend/public/image/colorized1.png"  />
+//                       <img src="/frontend/public/image/colorized2.png"  />`;
+//     container.add(div);
+//   }
+//   let container = document.getElementsByClassName("con");
+
+//     inputFile.addEventListener("click",function(event){
+//       event.preventDefault()
+//       var div = document.createElement("div")
+//       div.setAttribute("class","cont")
+//       div.innerHTML=` <img src="/frontend/public/image/colorized1.png"  />
+//                       <img src="/frontend/public/image/colorized2.png"  />`
+//       container.append(div)
+//   })
+
+//   return (<>
+//     <div className='con'>
+//       <div>
+//         <h1>SAR image colorizer</h1>
+//         <img src="" id='display' />
+//         <label htmlFor='input-file' onChange={Change()}>upload</label>
+//         <input type='file' accept='image/jpg,image/png,image.jpeg' id='input-file'></input>
+//         <button id='input-file'>colorize</button>
+//       </div>
+//     </div>
+//     <script src='/src/App.js'></script>
+//   </>
+//   )
+// }
+
+// export default App
